99642c2ce50042eca92748413e0c2bf363479cc7
Chakraphani Parthasarathy <shori_chakraphani@yahoo.com>
2025-07-16 23:41:18 +0530

    Linux scipt file updates

--- Citi2025/Linux/script.sh
+++ Citi2025/Linux/script.sh
@@ -1,1 +1,10 @@
 #Linux shell script
+#commands to search a file and extract words using delimiter
+#To get the default shell of logged in user
+awk -v u="$USER" -F: '$1 == u {print $NF}' /etc/passwd
+grep $USER /etc/passwd | grep -Eo '[^:]*$'
+
+#To get the current OS name and its release version
+uname -r
+grep -w -e "NAME" -e "VERSION" /etc/os-release
+
