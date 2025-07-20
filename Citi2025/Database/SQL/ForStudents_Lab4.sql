73f2703e7c4b139179b0da6cade5e31325b4a17e
Chakraphani Parthasarathy <shori_chakraphani@yahoo.com>
2025-07-20 22:51:24 +0530

    scripts added for Lab4

---
+++ Citi2025/Database/SQL/ForStudents_Lab4.sql
@@ -0,0 +1,33 @@
+#Aggregate Functions
+#Overview
+#In this lab we will practice using functions to count, find miniumum and maximum values etc.
+
+#EXERCISES
+--Count all authors
+
+
+--Calculate the average price of all titles
+
+
+--Count authors per city
+
+
+--Count titles per type
+
+
+--Count authors per state having more than 1 author
+
+
+--Find the maximum price of titles per type, having maximum price greater than 15
+
+
+--Find the minimum price of titles per type, having minimum price less than 20
+
+
+--Count publishers per country having more than 1 publisher
+
+
+--Find the average price of titles per type where the advance is greater than 5000 and sort them in descending order
+
+
+
---
+++ Citi2025/Database/SQL/Lab4_CasesinSession.sql
@@ -0,0 +1,10 @@
+#Aggregate Functions
+#Overview
+#In this lab we will practice using functions to count, find miniumum and maximum values etc.
+
+#EXERCISES
+--Find the minimum and maximum price of titles per type, where the type has more than 2 titles and the average price is greater than 15
+
+
+--Count publishers per country having more than 2 publishers and where the country name starts with 'U'
+
