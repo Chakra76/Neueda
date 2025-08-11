160401d5960e7c5fc2c8de6ad176bd9ae91cd004
Chakraphani Parthasarathy <shori_chakraphani@yahoo.com>
2025-08-11 09:47:56 +0000

    directory scan script changes

--- Citi2025/Linux/dirscan.sh
+++ Citi2025/Linux/dirscan.sh
@@ -1,10 +1,17 @@
-for i in $(ls -ltr) #./*
+#program that scan list of file from ls -ltr output in the current directory
+#and identify the type in the output, i.e., if it is a file or directory
+#and display the output accordingly
+
+for i in $(ls -ltr) #also can use ./* instead of $(ls -ltr)
 do
-    if [ -f "$i" ]
+    if [ -f "$i" ] #check if the value contained in i is a file 
     then
-        echo $(basename "$i")" is a file"
-    elif [ -d "$i" ]
+	#display only the filename but not the other attributes
+        #basename command display simply the filename and nothing else
+        echo $(basename "$i")" is a file" 
+    elif [ -d "$i" ] #check if the value contained in i is a directory 
     then
+	#basename command here displays only the directory name and not other attributes
         echo $(basename "$i")" is a directory"
     fi
 done
